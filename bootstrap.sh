649be1cbd89fd606a6bab50942300c2023005ac0
Ryan Olson <rmolson@gmail.com>
2016-10-21 01:08:50 -0600

    check for ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,18 @@
-ure running as regular user
+#!/usr/bin/env bash
+
+# Ensure running as regular user
 if [ $(id -u) -eq 0 ] ; then
     echo "Please run as a regular user"
     exit 1
 fi
 
 # Install newer version of Ansible
-# sudo apt-get -y install software-properties-common
-# sudo apt-add-repository -y ppa:ansible/ansible
-# sudo apt-get update
-# sudo apt-get -y install ansible
+if ! type ansible > /dev/null; then
+  sudo apt-get -y install software-properties-common
+  sudo apt-add-repository -y ppa:ansible/ansible
+  sudo apt-get update
+  sudo apt-get -y install ansible
+fi
 
 # Add nvidia-docker role from Ansible Galaxy
 # ansible-galaxy install ryanolson.nvidia-docker --roles-path=/tmp/roles
@@ -36,7 +40,7 @@ cat <<EOF > $playbook
 EOF
 
 # Execute playbook
-ansible-playbook -i "localhost," -c local $playbook
+ansible-playbook --ask-become-pass -i "localhost," -c local $playbook
 
 # cleanup
 rm -f $playbook
